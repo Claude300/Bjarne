a8ace38d31632ec0975d7dacaf8bd8d0e515a1ed
Claude300 <maurice3000@me.com>
2018-05-04 21:11:52 -0300

    initial commit

---
+++ project-1/project-1/src/project-1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "console.h"
+
+using namespace std;
+
+int main
+{
+    cout << "Enter a integer.\n";
+    int number = 0.0;
+    cin >> number '\n';
+    cout << "You enter the number:" << number << '\n';
+}
