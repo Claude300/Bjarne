7d57160f42dc15d447ac1642f106fd2dd01bf2a1
Claude300 <maurice3000@me.com>
2018-05-27 22:57:22 -0300

    testing push

--- project-6/project-6/src/project-6.cpp
+++ project-6/project-6/src/project-6.cpp
@@ -13,6 +13,7 @@ int main()
    cout << "  How are you? " << "I'm fine. I miss you a lots." << "\n";
    cout << "Do you come to Robertville this summer?" << '\n';
    cout << "Have a nice day! Come visit some day." << '\n';
-   cout << "Have you see ower friend " << friend_name << '?';
+   cout << "Have you see ower friend " << friend_name << "?\n";
+   cout << "Bye! Bye!";
    return 0;
 }
