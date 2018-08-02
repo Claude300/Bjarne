44f34b2b0315fa299278ad520770619ac7129bed
Claude300 <maurice3000@me.com>
2018-08-02 00:44:21 -0300

    commit august 2

--- project-6/project-6/src/project-6.cpp
+++ project-6/project-6/src/project-6.cpp
@@ -5,10 +5,11 @@ using namespace std;
 
 int main()
 {
-   cout << "Enter the name of the person you want to write to and the name of a friend (followed by 'enter'):\n";
+   cout << "Enter the name of the person you want to write to and " << "\n";
+   cout << "the name of a friend (followed by 'enter'):\n";
    string first_name = "???";// first_name is a variable of type string
    string friend_name = "???";
-   cin >> first_name >> friend_name;           // read characters into first_name
+   cin >> first_name >> friend_name;      // read characters into first_name
    cout << "Dear " << first_name << ",\n";
    cout << "  How are you? " << "I'm fine. I miss you a lots." << "\n";
    cout << "Do you come to Robertville this summer?" << '\n';
