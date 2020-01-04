c645c08ad8a58c7236cc4d719e5891951d73796b
Claude300 <maurice.lavigne@gmail.com>
2020-01-04 15:05:33 -0400

    another test

--- project-1/project-1/src/project-1.cpp
+++ project-1/project-1/src/project-1.cpp
@@ -16,5 +16,10 @@ using namespace std;
 int main
 {
     cout << "hello, world" << endl;
+    cout << "hello, world" << endl;
+    cout << "hello, world" << endl;
+    cout << "hello, world" << endl;
+    cout << "hello, world" << endl;
+    
     return 0;
 }
